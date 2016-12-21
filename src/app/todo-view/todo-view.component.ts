18c5d5c9982c5e797182f0b2e07da92da2246de9
Kirill Chilingarashvili <kirill.chilingarashvili@gmail.com>
2016-12-21 05:09:12 +0400

    added delete support

--- src/app/todo-view/todo-view.component.ts
+++ src/app/todo-view/todo-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { TodoService } from '../todo.service';
 
 @Component({
   selector: 'app-todo-view',
@@ -8,9 +9,16 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TodoViewComponent implements OnInit {
 
   @Input() item: any;
-  constructor() { }
+
+  constructor(private _todoService: TodoService) { }
 
   ngOnInit() {
+  }
+
+  public remove() {
+    if(confirm('Are you sure?')) {
+      this._todoService.remove(this.item.id);
+    }
   }
 
 }
