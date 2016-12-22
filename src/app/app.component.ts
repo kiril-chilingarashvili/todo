2ee6f81abd6c57b1136208af233514a9d73f4d63
Kirill Chilingarashvili <kirill.chilingarashvili@gmail.com>
2016-12-22 13:07:28 +0400

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, ViewEncapsulation } from '@angular/core';
 import { TITLE } from './title.token';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,61 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, OpaqueToken } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TodoModule } from './todo/todo.module';
+import { TodoComponent } from './todo/todo.component';
 import { TITLE } from './title.token';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { Child1Component } from './child1/child1.component';
+import { Child2Component } from './child2/child2.component';
+
+const appRoutes: Routes = [
+  { path: 'todo', component: TodoComponent },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'child1',
+        component: Child1Component,
+      },
+      {
+        path: 'child2',
+        component: Child2Component,
+      }
+    ]
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    Child1Component,
+    Child2Component
   ],
   imports: [
     TodoModule,
     BrowserModule,
+
+    RouterModule.forRoot(appRoutes)
     // FormsModule,
     // HttpModule
   ],
   providers: [
-    { provide: TITLE, useValue: 'TODO'}
-    ],
+    { provide: TITLE, useValue: 'TODO' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
